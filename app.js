8a3090b6b9e01eef1d26040ace02056cf8fe6496
sikandar hayat <codingfocuson@gmail.com>
2026-02-16 21:29:12 +0500

    js first assign

---
+++ app.js
@@ -0,0 +1,24 @@
+// java-script Assignment chapter 1 to 5;
+alert("java script first assignment");alert("well come ");alert("sikandar hayat khan");alert("bhara maja arha hy");console.log("smaj nhi Arha es me liku kia");
+// chapter 2 start;
+let username="sikandar";username="sikandar hayat khan";console.log(username);
+let message;message="bol jani kesa hy mazaa arha ";alert(message);
+let name="khan saab";let age=24;
+let status="single hu yr chara bnda hu";
+alert(name);alert(age);alert(status);
+let qana="mutton pulao mangwao yr ";let qana1="jani skht wali bhook lgi mangwa kuxch";let qana2="haa me hu khan saad";
+console.log(qana);console.log(qana1);console.log(qana2);
+let campus="smit student ";let campus1="learning web & app devolopment";let campus2="student of Sir Abdul Basit";let campus3="Abdul Basit Bharya ustad hy hummary";
+console.log(campus);console.log(campus1);console.log(campus2);console.log(campus3);
+// chapter 3 start;
+let a=10;let b=20;let num=a+b; let num1=a*b; let num2=b-a;console.log(num2);console.log(num1);console.log(num);
+// chapter 3 start;
+var userdata = +prompt("what is your age");
+if (userdata>=18){console.log("Adult")}else{console.log("sorry You are minor")};
+let userinfo=prompt("Enter your Marks");
+if(userinfo>=90){console.log("A+")}else if(userinfo>=80){console.log("A")}else if(userinfo>=70){console.log("B")}else if(userinfo>=60){console.log("c")}else{console.log("Fail")};
+let acd=10;let bn=10;let vm=acd*bn;console.log(num);
+
+
+
+
